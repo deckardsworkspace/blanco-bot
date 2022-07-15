63acdc424dd7db04d2c054c5677680c404b9d17c
Jared Dantis <jareddantis@gmail.com>
2022-07-15 18:14:38 +0800

    cogs: Pass db to PlayerCog

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,6 +1,12 @@
 from discord.ext.commands import Bot
+from os import environ
+from utils.database import Database
 from .player import PlayerCog
 
 
 def setup(bot: Bot):
-    bot.add_cog(PlayerCog(bot))
+    # Create Database instance
+    db = Database(environ['DB_FILENAME'])
+
+    # Add cogs
+    bot.add_cog(PlayerCog(bot, db))
--- cogs/player.py
+++ cogs/player.py
@@ -1,13 +1,34 @@
-from discord.ext.commands import Bot, Cog, command, Context
+from discord.ext.commands import Cog, command, Context
+from lavalink import add_event_hook, DefaultPlayer
+from lavalink.events import *
+from typing import Optional
+from utils.database import Database
+from utils.lavalink import init_lavalink
+from utils.lavalink_bot import LavalinkBot
 
 
 class PlayerCog(Cog):
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: LavalinkBot, db: Database):
         self.bot = bot
+        self.db = db
 
-    @Cog.listener()
-    async def on_ready(self):
-        print(f'Loaded {__name__} cog')
+        # Create Lavalink client instance
+        if not hasattr(bot, 'lavalink'):
+            bot.lavalink = init_lavalink(bot.user.id)
+
+        # Listen to Lavalink events
+        add_event_hook(self.on_lavalink_event)
+
+        print(f'Loaded cog: {self.__class__.__name__}')
+    
+    async def on_lavalink_event(self, event: Event):
+        pass
+
+    async def get_player(self, guild_id: int) -> Optional[DefaultPlayer]:
+        """
+        Get the player for a guild.
+        """
+        return self.bot.lavalink.player_manager.get(guild_id)
     
     @command(name='play', aliases=['p'])
     async def play(self, ctx: Context, *, url: str):
