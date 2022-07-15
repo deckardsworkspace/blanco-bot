f70707aedd081367b6ca30a9288f371d49a92615
Jared Dantis <jareddantis@gmail.com>
2022-07-16 03:35:03 +0800

    main: Remove logic for message content

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from nextcord import DiscordException, Intents, Message
-from nextcord.ext.commands import Context
+from nextcord import Intents
 from os import environ
 from utils.lavalink_bot import LavalinkBot
 
 
 # Create bot instance
-intents = Intents.default()
-intents.message_content = True
-client = LavalinkBot(command_prefix=environ['DISCORD_BOT_PREFIX'], intents=intents)
+client = LavalinkBot(intents=Intents.none())
 
 # Event listeners
 @client.event
@@ -15,18 +12,7 @@ async def on_ready():
     print('Logged in as {0}!'.format(client.user))
     client.load_extension('cogs')
 
-@client.event
-async def on_message(message: Message):
-    # Ignore messages from self
-    if message.author == client.user:
-        return
-
-    try:
-        await client.process_commands(message)
-    except Exception as e:
-        await message.reply(f'Error while processing command: {e}')
-
 # Run client
 if __name__ == '__main__':
-    print('Starting bot with prefix: {0}'.format(environ['DISCORD_BOT_PREFIX']))
+    print('Starting bot...')
     client.run(environ['DISCORD_BOT_TOKEN'])
