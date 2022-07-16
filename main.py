7d9e0fa0f0e815b4a78219d3e856597ec375f642
Jared Dantis <jareddantis@gmail.com>
2022-07-16 14:58:51 +0800

    main: Send slash cmd errors to channel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from nextcord import Intents
+from nextcord import Intents, Interaction
 from os import environ
+from utils.jockey_helpers import create_error_embed
 from utils.lavalink_bot import LavalinkBot
 
 
@@ -11,6 +12,10 @@ client = LavalinkBot(intents=Intents.default())
 async def on_ready():
     print('Logged in as {0}!'.format(client.user))
     client.load_extension('cogs')
+
+@client.event
+async def on_application_command_error(itx: Interaction, error: Exception):
+    await itx.channel.send(embed=create_error_embed(error))
 
 # Run client
 if __name__ == '__main__':
