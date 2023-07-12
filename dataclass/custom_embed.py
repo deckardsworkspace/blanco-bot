eed26474c5b81e0fb002b582cf56e3689a78901d
Jared Dantis <jareddantis@gmail.com>
2023-07-13 01:33:23 +0800

    custom_embed: Replace EmptyEmbed with None

--- dataclass/custom_embed.py
+++ dataclass/custom_embed.py
@@ -1,36 +1,35 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 from nextcord import Color, Embed, Message
-from nextcord.embeds import EmptyEmbed
 from nextcord.ext.commands import Context
-from typing import List, Union
+from typing import List, Optional, Union
 
 
 @dataclass
 class CustomEmbed:
     # All optional
-    title: str = EmptyEmbed
+    title: Optional[str] = None
     color: Color = Color.og_blurple()
-    description: Union[str, List[str]] = EmptyEmbed
+    description: Optional[Union[str, List[str]]] = None
     fields: List[List[str]] = field(default_factory=list)
     inline_fields: bool = False
-    thumbnail_url: str = EmptyEmbed
-    image_url: str = EmptyEmbed
+    thumbnail_url: Optional[str] = None
+    image_url: Optional[str] = None
 
     # Header and footer
-    header: str = EmptyEmbed
-    header_url: str = EmptyEmbed
-    header_icon_url: str = EmptyEmbed
-    footer: str = EmptyEmbed
-    footer_icon_url: str = EmptyEmbed
+    header: Optional[str] = None
+    header_url: Optional[str] = None
+    header_icon_url: Optional[str] = None
+    footer: Optional[str] = None
+    footer_icon_url: Optional[str] = None
     timestamp_now: bool = False
 
     # Create embed
     def __post_init__(self):
         # Can't specify header/footer icons without header/footer names
-        if self.header is EmptyEmbed and self.header_icon_url is not EmptyEmbed:
+        if self.header is None and self.header_icon_url is not None:
             raise ValueError("Can't specify header icon without header text.")
-        if self.footer is EmptyEmbed and self.footer_icon_url is not EmptyEmbed:
+        if self.footer is None and self.footer_icon_url is not None:
             raise ValueError("Can't specify footer icon without footer text.")
 
         # Create embed object
@@ -40,15 +39,15 @@ class CustomEmbed:
         embed = Embed(title=self.title, description=description, color=self.color)
         
         # Set embed parts
-        if self.header is not EmptyEmbed:
+        if self.header is not None:
             embed.set_author(name=self.header)
-        if self.thumbnail_url is not EmptyEmbed and self.thumbnail_url != '':
+        if self.thumbnail_url is not None and self.thumbnail_url != '':
             embed.set_thumbnail(url=self.thumbnail_url)
-        if self.image_url is not EmptyEmbed:
+        if self.image_url is not None:
             embed.set_image(url=self.image_url)
-        if self.header is not EmptyEmbed:
+        if self.header is not None:
             embed.set_author(name=self.header, url=self.header_url, icon_url=self.header_icon_url)
-        if self.footer is not EmptyEmbed:
+        if self.footer is not None:
             embed.set_footer(text=self.footer, icon_url=self.footer_icon_url)
         if len(self.fields):
             for field in self.fields:
