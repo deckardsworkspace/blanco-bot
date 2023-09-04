1079be9ce06c92f30fc6a132284b1a8c7234c745
Jared Dantis <jareddantis@gmail.com>
2023-09-04 23:12:02 +0800

    cogs: Only import BlancoBot if type checking

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,9 +1,11 @@
-from utils.blanco import BlancoBot
+from typing import TYPE_CHECKING
 from .debug import DebugCog
 from .player import PlayerCog
+if TYPE_CHECKING:
+    from utils.blanco import BlancoBot
 
 
-def setup(bot: BlancoBot):
+def setup(bot: 'BlancoBot'):
     # Add cogs
     bot.add_cog(DebugCog(bot))
     bot.add_cog(PlayerCog(bot))
