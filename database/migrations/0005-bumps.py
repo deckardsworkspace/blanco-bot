e18f13f79baa9cd8b48a2fbaa9e89191379ebc6f
Max Reiter <max.m.reiter@gmail.com>
2024-03-06 12:01:11 -0700

    add migration for bumps

---
+++ database/migrations/0005-bumps.py
@@ -0,0 +1,42 @@
+"""
+Create tables for guild bumps and alter player_settings where needed.
+"""
+
+# pylint: disable=invalid-name
+
+from sqlite3 import OperationalError
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sqlite3 import Connection
+
+
+def run(con: 'Connection'):
+    """
+    Run the migration.
+    """
+    cur = con.cursor()
+
+    try:
+        cur.execute('''
+            ALTER TABLE player_settings ADD COLUMN bump_interval INTEGER NOT NULL DEFAULT 20
+        ''')
+
+        cur.execute('''
+            ALTER TABLE player_settings ADD COLUMN last_bump INTEGER NOT NULL
+        ''')
+
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS bumps (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                idx INTEGER NOT NULL,
+                guild_id INTEGER NOT NULL,
+                url TEXT NOT NULL,
+                    
+                UNIQUE(guild_id, idx)
+            )
+        ''')
+
+        con.commit()
+    except OperationalError:
+        pass
